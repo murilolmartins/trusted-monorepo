e3b3a2b97552de9cf5cb8e22ca21c924d4f720bc
Murilo Martins <murilolmartins1@gmail.com>
2023-07-23 13:08:16 -0300

    feat: schedualing-tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // This will lint and format TypeScript and JavaScript files
   './**/*.(ts|tsx|js|jsx)': (filenames) => [
     `yarn prettier --write ${filenames.join(' ')}`,
-    `yarn eslint --fix ${filenames.join(' ')}`
+    `yarn eslint --fix ${filenames.join(' ')} --max-warnings=0`
   ],
 
   './**/*.(ts|tsx)': (filenames) => [
